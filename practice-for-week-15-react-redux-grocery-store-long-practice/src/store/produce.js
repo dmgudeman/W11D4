0a0b3043462ac2b0d26239cdd83fe8a923d80019
AA Student <appacademy@Mini-34.local>
2023-04-13 15:07:57 -0700

    finish phase 2

---
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/produce.js
@@ -0,0 +1,25 @@
+import produceData from '../mockData/produce.json';
+const POPULATE = "produce/POPULATE"
+
+export default function produceReducer(state = {}, action) {
+    switch (action.type) {
+        case POPULATE:
+            const newObject = {}
+            action.produce.forEach((produce)=>{
+                newObject[produce.id]=produce;
+            })
+            return newObject;
+        default:
+            return state;
+
+    }
+}
+
+export function populateProduce() {
+
+    return {
+       type: POPULATE,
+       produce: produceData
+    }
+
+}
